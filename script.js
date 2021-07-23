009db68a7160ada172da3ea394422d5f70bd12f3
Jacob Von Ogden <jvon1197@gmail.com>
2021-07-22 21:43:11 -0400

    Landing and about page added

---
+++ script.js
@@ -0,0 +1,15 @@
+
+/*
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+              }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+*/
